7be3ed6f982d42ec441eb935b64c2a3063da0697
Robert S. <RobertS@eostar.com>
2017-01-01 04:28:39 -0500

    build fix

---
+++ Faciem/Faciem/Data/apikey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faciem.Data
+{
+	public static class ApiKey
+	{
+
+		public const string OXFORD_API_KEY = "9b90f7f4a3de4d928d8ed2105806d263";
+		public const string MOBILE_CENTER_API_KEY = "60ac7aa8-5d04-4bfb-a056-9c40afa71105";
+
+
+	}
+}
